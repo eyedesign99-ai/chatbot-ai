48fca6bb39bc1372876dea00f56cd285ddfa14ef
eyedesign99-ai <eyedesign99@gmail.com>
2025-10-25 16:16:10 +0700

    Add files via upload

--- static/script.js
+++ static/script.js
@@ -37,7 +37,7 @@ function appendMessage(sender, message){
 function setLoading(val){
     const btn = document.querySelector(".send-btn");
     btn.innerHTML = val ? `<img src="/static/icon.png" class="loading-icon" alt="...">`
-                        : `<svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" fill="white" viewBox="0 0 24 24"><path d="M4 12l1.41 1.41L11 7.83V20h2V7.83l5.59 5.58L20 12l-8-8-8 8z"/></svg>`;
+                        : `<svg xmlns="/static/icon.png" class="loading-icon" alt="...">`;
 }
 
 function typeResponse(sender, message){
